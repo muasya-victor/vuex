7575304c3e81e44be87b5b4526a988226b583e94
victor muasya <83345578+muasya-victor@users.noreply.github.com>
2022-06-08 21:50:50 +0300

    added post component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="hello flex flex-wrap p-2 justify-center ">
-    <div v-for="(blog, index) in $store.state.blogs " :key="index"
-         class="ring-1 m-1 rounded-2xl w-11/12 md:w-2/5 " >
-      <div class="flex justify-between md:px-5 px-2 items-center underline text-blue-500" >
-          <div class="flex flex-col items-start md:px-5 px-2 tracking-wider">
-            <h3 class="text-xl text-blue-400 capitalize">{{blog.category}}</h3>
-            <h3 class="text-xl text-blue-400 capitalize">{{blog.title}}</h3>
-            <p class="tracking-wide">{{ blog.body }}</p>
-          </div>
-      </div>
-<!--      {{$store.state.blogs }}-->
-    </div>
-  </div>
+ <div>
+   <div class="hello flex flex-wrap p-2 justify-center ">
+     <div v-for="(blog, index) in $store.state.blogs " :key="index"
+          class="ring-1 m-1 rounded-2xl w-11/12 md:w-2/5 " >
+       <div class="flex justify-between md:px-5 px-2 items-center underline text-blue-500" >
+         <div class="flex flex-col items-start md:px-5 px-2 tracking-wider">
+           <h3 class="text-xl text-blue-400 capitalize">{{blog.category}}</h3>
+           <h3 class="text-xl text-blue-400 capitalize">{{blog.title}}</h3>
+           <p class="tracking-wide">{{ blog.body }}</p>
+         </div>
+       </div>
+       <!--      {{$store.state.blogs }}-->
+     </div>
+   </div>
+
+<!--   ------------------------------------------------------------------------------------------------------------->
+   <PostBlog/>
+<!--   ------------------------------------------------------------------------------------------------------------->
+
+ </div>
 
 </template>
 
 <script>
+import PostBlog from "@/components/PostBlog";
+
 export default {
   name: 'HelloWorld',
-
+  components : {
+    PostBlog
+  }
 
 }
 </script>
---
+++ src/components/PostBlog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    yoo
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "PostBlog",
+
+}
+</script>
+
+<style scoped>
+
+</style>
